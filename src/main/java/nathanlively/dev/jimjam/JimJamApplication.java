66ac8b5ad2e90d7922df010da62c8e8382074b1d
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-10-06 11:39:53 -0500

    first commit

---
+++ src/main/java/nathanlively/dev/jimjam/JimJamApplication.java
@@ -0,0 +1,13 @@
+package nathanlively.dev.jimjam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimJamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JimJamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nathanlively/dev/jimjam/JimJamApplicationTests.java
@@ -0,0 +1,13 @@
+package nathanlively.dev.jimjam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JimJamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
