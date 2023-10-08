722469609cfb1c255b66d35001399817f286759b
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-10-08 17:01:00 -0500

    Refactor fragments and enhance UI with user location and Hobis quotes
    Renamed "iss-fragments" to "fragments" to clarify that these are general fragments used in the project, not only related to the ISS. Enhanced the UI by replacing the grid layout with flex layout for displaying latitude and longitude details. Added id attributes to latitude and longitude spans for ISS and user for easier access. Removed unnecessary comments. Included a new function to calculate the distance between the user and ISS and display it on the page. Also, added a new HxRequest named "hobis" that pulls up random quotes from "Hobis says.txt" and displays them on the main page.

--- src/main/java/nathanlively/dev/jimjam/HomeController.java
+++ src/main/java/nathanlively/dev/jimjam/HomeController.java
@@ -2,6 +2,7 @@ package nathanlively.dev.jimjam;
 
 import io.github.wimdeblauwe.hsbt.mvc.HxRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import nathanlively.dev.jimjam.ip.astronomy.AstronomyResponse;
 import nathanlively.dev.jimjam.ip.IpGeoLocationService;
 import nathanlively.dev.jimjam.iss.IssService;
@@ -9,15 +10,26 @@ import nathanlively.dev.jimjam.iss.location.IssLocationResponse;
 import nathanlively.dev.jimjam.iss.pop.IssPopResponse;
 import nathanlively.dev.jimjam.weather.WeatherResponse;
 import nathanlively.dev.jimjam.weather.WeatherService;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Mono;
+import java.time.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/")
 @RequiredArgsConstructor
+@Log4j2
 public class HomeController {
 
     private final IpGeoLocationService ipGeoLocationService;
@@ -50,7 +62,7 @@ public class HomeController {
     public String issLocation(Model model) {
         Mono<IssLocationResponse> issLocationResponseMono = issService.getLocation();
         model.addAttribute("issLocation", issLocationResponseMono.block());
-        return "iss-fragments :: issLocation";
+        return "fragments :: issLocation";
     }
 
     @HxRequest
@@ -58,7 +70,47 @@ public class HomeController {
     public String issPopulation(Model model) {
         Mono<IssPopResponse> issPopResponseMono = issService.getPopulation();
         model.addAttribute("issPop", issPopResponseMono.block());
-        return "iss-fragments :: issPopulation";
+        return "fragments :: issPopulation";
+    }
+
+    @HxRequest
+    @GetMapping("hobis")
+    public String hobis(Model model) {
+
+        List<String> quotes = new ArrayList<>();
+
+        try {
+            Resource resource = new ClassPathResource("static/Hobis says.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+
+            String line;
+            StringBuilder quoteBuilder = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                if (!line.startsWith(" ")) {
+                    if (!quoteBuilder.isEmpty()) {
+                        quotes.add(quoteBuilder.toString());
+                        quoteBuilder.setLength(0);
+                    }
+                    quoteBuilder.append(line);
+                } else {
+                    quoteBuilder.append("\n").append(line.trim());
+                }
+            }
+
+            if (!quoteBuilder.isEmpty()) {
+                quotes.add(quoteBuilder.toString());
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+
+        LocalDate today = LocalDate.now();
+        Random seed = new Random(today.atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli());
+
+        int index = seed.nextInt(quotes.size());
+        model.addAttribute("quote", quotes.get(index));
+
+        return "fragments :: hobis";
     }
 
 }
